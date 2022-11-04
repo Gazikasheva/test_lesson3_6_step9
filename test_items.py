9fc03c451ad552ae1ee2dcdebfa3bc96f394afa2
Gazikasheva <32996602+Gazikasheva@users.noreply.github.com>
2022-11-04 21:41:58 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,4 +6,5 @@ link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_item_has_add_to_cart_button (browser):
     browser.get (link)
-    assert browser.find_element (By.CLASS_NAME, "btn-add-to-basket").is_displayed (), f"'Add to basket' button is not displayed"
+    assert len (browser.find_elements (By.CLASS_NAME, "btn-add-t-basket")) >0, f"'Add to basket' button is not displayed"
+
