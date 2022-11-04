9a845c9e16607c7e092ff8c54ab7c3fc0845117a
Gazikasheva <32996602+Gazikasheva@users.noreply.github.com>
2022-11-04 21:20:11 +0300

    Update test_items.py
    small description changes

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ from selenium.webdriver.common.by import By
 
 link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_reg1 (browser):
+def test_item_has_add_to_cart_button (browser):
     browser.get (link)
     assert browser.find_element (By.CLASS_NAME, "btn-add-to-basket").is_displayed (), f"'Add to basket' button is not displayed"
